05e16a6b349fcc6ca0a80477da37aa86f58eb5de
Sam McAlilly <sammcalilly@gmail.com>
2020-07-12 20:51:28 -0500

    ugh

--- backend/app/wsgi.py
+++ backend/app/wsgi.py
@@ -8,11 +8,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings.base')
 print('wsgi.py')
 print(os.environ.get('DJANGO_SETTINGS_MODULE'))
+
+print(sys.path)
 
 application = get_wsgi_application()
