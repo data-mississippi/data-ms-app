a4a2899a822a9e053ee5ee8d85c90defa803523a
Sam McAlilly <sammcalilly@gmail.com>
2021-02-25 20:37:52 -0600

    county kiosk

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import GeneralMap from './maps/GeneralMap'
+import React, { useState } from 'react'
+import CountyBorderMap from './maps/CountyBorderMap'
+
+function CountyDetails({ county }) {
+    console.log('county', county)
+
+    return (
+        <div className="">
+            <h2 className="text-4xl">{county['NAME']} County</h2>
+            <p>2000 Population: {county['POP2000']}</p>
+        </div>
+    )
+}
 
 function Home() {
+    const [county, setCounty] = useState(null)
+
     return (
-    <div className='grid grid-cols-6'>
-        <div className="col-span-2">
-            <GeneralMap />
+        <div className='grid grid-cols-6 mx-4'>
+            <div className="col-span-2 m-2 rounded-lg border-2 border-blue-900 bg-red-50 p-4">
+                <CountyBorderMap onChooseCounty={(county) => setCounty(county)} />
+            </div>
+            
+            <div className="col-span-2 rounded-lg border-2 border-blue-900 bg-red-50 px-4 py-2 m-2">
+                <h1 className="text-xl text-right text-blue-900 font-mono">County Kiosk</h1>
+                {county ? <CountyDetails county={county} /> : <p className="text-right">Click on a county to see more.</p>}
+            </div>
         </div>
-        
-        <div className="col-span-2">counties</div>
-    </div>
-      
     )
 }
 
---
+++ frontend/src/maps/CountyBorderMap.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import BaseMap from './BaseMap'
+import { GeoJSON } from 'react-leaflet'
+
+
+function CountyBorderMap({ children, onChooseCounty }) {
+    const [countyBorders, setCountyBorders] = useState(null)
+    const [boundary, setBoundary] = useState(null)
+    const grayPurpleFill = {
+        fillColor: '#daf0ce', 
+        weight: 0.5,
+        opacity: 0.4,
+        color: '#666',
+        fillOpacity: 0.5
+    }
+
+    function onBoundaryMouseover(e) {
+        const layer = e.target
+
+        layer.setStyle({
+            fillOpacity: 1,
+        });
+    }
+
+    function resetBoundaryStyle(e) {
+        setBoundary(() => null)
+        e.target.setStyle(grayPurpleFill)
+    }
+
+    function onCountyClick(e) {
+        const layer = e.target
+
+        const layerFeature = (layer && layer.feature && layer.feature.properties) 
+                                ? layer.feature.properties 
+                                : null
+
+        // send the information about the feature to the parent component
+        onChooseCounty(layerFeature)
+    }
+
+    function eventHandlersOnEachFeature(feature, layer) {
+        // this doesn't seem right...
+        // update: it's not! see https://reactjs.org/docs/refs-and-the-dom.html
+        // or maybe it is? see node_modules/@types/leaflet/index.d.ts line 1209
+    
+        layer.on({
+          mouseover: onBoundaryMouseover,
+          mouseout: resetBoundaryStyle,
+          click: onCountyClick
+        })
+    }
+
+    useEffect(() => {
+        fetch(`/counties/borders/000/`, {
+            headers: {'Accept': 'application/json'}
+          }).then((res) => res.json())
+            .then((counties) => {
+                console.log('counties', counties)
+                setCountyBorders(counties.geojson)
+            })
+
+    }, [setCountyBorders])
+
+    
+
+    return (
+        <BaseMap 
+            center={[32.56, -89.89]}
+            zoom={7}>
+            {countyBorders 
+                && <GeoJSON
+                    key='states-layer'
+                    data={countyBorders}
+                    style={grayPurpleFill}
+                    onEachFeature={eventHandlersOnEachFeature} />
+            }
+        </BaseMap>
+    )
+}
+
+export default CountyBorderMap
