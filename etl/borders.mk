c32b1afeffab4f6736b644686bd33e8b67f2924f
Sam McAlilly <sammcalilly@gmail.com>
2021-05-06 17:01:52 -0500

    clean up

--- etl/borders.mk
+++ etl/borders.mk
@@ -1,6 +1,5 @@
-output/county_borders_2020.geojson: tl_2020_28_county20.shp
+../frontend/public/geojson/county_borders_2020.geojson: tl_2020_28_county20.shp
 	ogr2ogr -f GeoJSON $@ $<
-	#cp ../frontend/public/geojson/county_borders_2020.geojson ../backend/counties/raw/county_borders_2020.geojson
 
 .INTERMEDIATE : tl_2020_28_county20.shp
 tl_2020_28_county20.shp: county_borders_2020.zip
--- etl/fips.mk
+++ etl/fips.mk
@@ -1,4 +1,2 @@
-output/fips.csv:
+../backend/counties/raw/fips.csv:
 	curl -o $@ https://www2.census.gov/geo/docs/reference/codes/files/st28_ms_cou.txt
-
-	cp $@ ../backend/counties/raw/county_fips.csv
